9ab118ef118785b1d78dcec228dc5cad04cd4cf7
TongZhangzt <Toz7@pitt.edu>
2016-12-10 18:42:38 -0500

    change style

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,3 +1,18 @@
+var margin = {
+        top: 50,
+        right: 0,
+        bottom: 100,
+        left: 30
+    },
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom,
+    gridSize = Math.floor(width / 31),
+    legendElementWidth = gridSize * 2,
+    buckets = 9,
+    colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    days = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+    times = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+
 function date(d) {
     if (d < 10)
         return "0" + d;
